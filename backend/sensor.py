c3995f84b24f491dc81d9d358089157fae0247ea
zayutaha <zayaan.taha@icloud.com>
2025-11-21 15:35:31 +0530

    Basic structure for backend

---
+++ backend/sensor.py
@@ -0,0 +1,22 @@
+class SensorReading:
+   def __init__(self, nitrogen: int, potassium: int, phosphorus: int, soil_moisture: int):
+        self.nitrogen = nitrogen
+        self.potassium = potassium
+        self.phosphorus = phosphorus
+        self.soil_moisture = soil_moisture
+
+
+def parse(data) -> SensorReading:
+    feeds = extract_feeds(data)
+    return SensorReading(
+        nitrogen=int(feeds['field1']),
+        potassium=int(feeds['field3']),
+        phosphorus=int(feeds['field2']),
+        soil_moisture=int(feeds['field4'])
+    )
+
+
+def extract_feeds(data):
+   return data["feeds"][0]
+
+
